5dcc3312ff940d47404c9edbaaf7b2ed5d00b369
Jonas Horstmann <jonas.horstmann2804@gmail.com>
2025-08-25 18:05:06 +0200

    Add expressive code for better code blocks

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), expressiveCode()]
 });
